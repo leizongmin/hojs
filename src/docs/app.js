8c8248d54704006cc2177588dae0ced61a91116c
Zongmin Lei <leizongmin@gmail.com>
2016-03-29 18:28:20 +0800

    更新文档样式

--- src/docs/app.js
+++ src/docs/app.js
@@ -34,53 +34,55 @@ function makeSchemaDocs(schema) {
       <h2 className="title"><a href={`#${schema.id}`}>{schema.route} {schema.title}</a></h2>
       <div className="group">分组：{schema.group}</div>
       <div className="description">{schema.description}</div>
-      <div>请求参数：</div>
-      <ul>
-      {
-        Object.keys(schema.params).map(name => {
-          const info = schema.params[name];
-          return (
-            <li key={name}>
-              <span className="param-name">{name}</span>
-              <span className="param-type">{info.type}</span>
-              <span className="param-comment">{info.comment}</span>
-            </li>
-          );
-        })
-      }
-      </ul>
-      <div>必须参数：</div>
-      <ul>
+      <div className="block">
+        <div className="block-title">请求参数</div>
+        {
+          Object.keys(schema.params).map(name => {
+            const info = schema.params[name];
+            return (
+              <div className="param-item" key={name}>
+                <span className="param-name">{name}</span>
+                <span className="param-type">{info.type}</span>
+                <span className="param-comment">{info.comment}</span>
+              </div>
+            );
+          })
+        }
+      </div>
+      <div className="block">
+        <div className="block-title">必须参数</div>
       {
         schema.required.map(name => {
           return (
-            <li key={name}>
+            <div key={name}>
               <span className="param-name">{name}</span>
-            </li>
+            </div>
           );
         })
       }
       {
         schema.requiredOneOf.map(names => {
           return (
-            <li key={names}>
+            <div key={names}>
               <span className="param-name">{names.join(', ')} 其中一个</span>
-            </li>
-          );
-        })
-      }
-      </ul>
-      <div>使用示例</div>
-      {
-        schema.examples.map(({input, output}, i) => {
-          return (
-            <div className="example" key={i}>
-              <pre key={`input-${i}`}>input = {jsonStringify(input, 2)};</pre>
-              <pre key={`output-${i}`}>output = {jsonStringify(output, 2)};</pre>
             </div>
           );
         })
       }
+      </div>
+      <div className="block">
+        <div className="block-title">使用示例</div>
+        {
+          schema.examples.map(({input, output}, i) => {
+            return (
+              <div className="example" key={i}>
+                <pre key={`input-${i}`}>input = {jsonStringify(input, 2)};</pre>
+                <pre key={`output-${i}`}>output = {jsonStringify(output, 2)};</pre>
+              </div>
+            );
+          })
+        }
+      </div>
     </div>
   );
 }
