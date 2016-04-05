8047e6a219046669c9cf53d5ab809130494fcee7
Zongmin Lei <leizongmin@gmail.com>
2016-04-05 11:01:30 +0800

    将部分函数转移到utils.js

--- src/lib/index.js
+++ src/lib/index.js
@@ -17,62 +17,12 @@ import Schema from './schema';
 import registerDefaultTypes from './default_types';
 import registerDefaultErrors from './default_errors';
 import {core as debug} from './debug';
-
-
-function getCallerSourceLine(dir) {
-  dir = resolvePath(dir);
-  const stack = (new Error()).stack.split('\n').slice(1);
-  for (let line of stack) {
-    line = line.trim();
-    if (line.replace(/\\/g, '/').indexOf(dir) !== -1) {
-      const s = line.match(/\((.*)\)\s*$/);
-      if (s) {
-        return {
-          relative: s[1].slice(dir.length + 1),
-          absolute: s[1],
-        };
-      }
-    }
-  }
-  return {relative: null, absolute: null};
-}
-
-function createRouter() {
-  return express.Router({
-    caseSensitive: true,
-    mergeParams: true,
-    strict: true,
-  });
-}
-
-function mergeParams(...list) {
-  const ret = {};
-  for (const item of list) {
-    if (item && typeof item === 'object') {
-      for (const i in item) {
-        ret[i] = item[i];
-      }
-    }
-  }
-  return ret;
-}
-
-function wrapAsyncMiddleware(fn) {
-  if (fn.length === 2) {
-    return function (req, res, next) {
-      let p = null;
-      try {
-        p = fn(req, res);
-      } catch (err) {
-        return next(err);
-      }
-      p.then(ret => next());
-      p.catch(err => next(err));
-    };
-  } else {
-    return fn;
-  }
-}
+import {
+  getCallerSourceLine,
+  createRouter,
+  mergeParams,
+  wrapAsyncMiddleware,
+} from './utils';
 
 
 export default class Hojs extends ProjectCore {
