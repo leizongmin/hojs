ff5ea03afc00c015381317a2e519cdb3079fcd4c
Zongmin Lei <leizongmin@gmail.com>
2016-05-11 16:48:30 +0800

    api.override.get() 允许重载API

--- src/lib/extends/register.js
+++ src/lib/extends/register.js
@@ -13,14 +13,46 @@ import {core as debug} from '../debug';
 
 export default function () {
 
+  this.api.override = {};
+
+  const register = (method, path, strict = true) => {
+
+    const s = new Schema(method, path, getCallerSourceLine(this.config.get('api.path')));
+    const s2 = this.api.$schemaMapping[s.key];
+
+    if (strict) {
+      assert(!s2, `try to register API ${s.key} at file ${s.options.sourceFile.absolute} but was already registered at file ${s2 && s2.options.sourceFile.absolute}`);
+    }
+
+    if (s2) {
+      removeSchemas(s.key);
+      debug('override API: %s %s at %s', method, path, s.options.sourceFile.absolute);
+    }
+
+    this.api.$schemas.push(s);
+    this.api.$schemaMapping[s.key] = s;
+    return s;
+
+  };
+
+  const removeSchemas = (key) => {
+
+    for (let i = 0; i < this.api.$schemas.length; i++) {
+      const s = this.api.$schemas[i];
+      if (s.key === key) {
+        this.api.$schemas.splice(i, 1);
+        i--;
+      }
+    }
+
+  };
+
   for (const method of Schema.SUPPORT_METHOD) {
     this.api[method] = (path) => {
-      const s = new Schema(method, path, getCallerSourceLine(this.config.get('api.path')));
-      const s2 = this.api.$schemaMapping[s.key];
-      assert(!s2, `try to register API ${s.key} at file ${s.options.sourceFile.absolute} but was already registered at file ${s2 && s2.options.sourceFile.absolute}`);
-      this.api.$schemas.push(s);
-      this.api.$schemaMapping[s.key] = s;
-      return s;
+      return register(method, path, true);
+    };
+    this.api.override[method] = (path) => {
+      return register(method, path, false);
     };
   }
 
