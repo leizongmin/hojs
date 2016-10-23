ccd1ad2303720a86eb8d5d0844835afe48dae949
Zongmin Lei <leizongmin@gmail.com>
2016-10-23 22:35:15 +0800

    更新eslint配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,3 @@
 module.exports = {
-  'parser': 'babel-eslint',
-  'parserOptions': {
-    'sourceType': 'module',
-    'allowImportExportEverywhere': false,
-  },
-  'root': true,
-  'extends': 'lei/mocha',
-  'rules': {
-    'generator-star-spacing': 'off',
-    'require-yield': 'off',
-  },
+  extends: 'lei/mocha',
 };
